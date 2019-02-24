33fac1f8c48759060f2110a4973b380781f66d6e
miahlen <miahlen@gmail.com>
2019-02-24 17:48:00 +0100

    Add normalize_quaternion method to quaternion_algebra.py
    Will return a normalized version of the input quaternion, unless the norm of the input quaternion is zero upon which an exception will be raised

--- quaternion_algebra.py
+++ quaternion_algebra.py
@@ -5,3 +5,16 @@ import numpy as np
 
 def get_quaternion_norm(q):
     return np.sqrt(q.w**2 + q.x**2 + q.y**2 + q.z**2)
+
+def normalize_quaternion(q):
+    norm = get_quaternion_norm(q)
+
+    # Make sure norm is not zero before normalizing the quaternion
+    if norm==0.0:
+        raise Exception("Quaternion norm equals zero")
+
+    q.w /= norm
+    q.x /= norm
+    q.y /= norm
+    q.z /= norm
+    return q
