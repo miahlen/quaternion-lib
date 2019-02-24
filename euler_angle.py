7cfaa72ab9787b7ddcc2af68862db31075564d88
miahlen <miahlen@gmail.com>
2019-02-24 19:27:42 +0100

    Add operator overloading method __str__ to EulerAngle class
    To be able to easily print an euler angle's values

--- euler_angle.py
+++ euler_angle.py
@@ -3,3 +3,6 @@ class EulerAngle():
         self.roll = roll
         self.pitch = pitch
         self.yaw = yaw
+
+    def __str__(self):
+        return "("+str(self.roll)+", "+str(self.pitch)+", "+str(self.yaw)+")"
