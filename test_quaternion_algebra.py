7081cc919262a0a7790c7203abcef2328728ce6c
miahlen <miahlen@gmail.com>
2019-02-24 17:35:43 +0100

    Implement test_normalize_quaternion method in TestQuaternionAlgebra class
    Will assert that the quaternion's norm after the normalization call will be 1 and that the expected quaternion is returned

--- test_quaternion_algebra.py
+++ test_quaternion_algebra.py
@@ -1,10 +1,15 @@
 import unittest
 import quaternion_algebra
+from quaternion import Quaternion
+import numpy as np
 
 class TestQuaternionAlgebra(unittest.TestCase):
 
     def test_normalize_quaternion(self):
-        raise NotImplementedError
+        q = Quaternion(-2.1,0.0,0.0,0.0)
+        q = quaternion_algebra.normalize_quaternion(q)
+        self.assertTrue(np.sqrt(q.w**2 + q.x**2 + q.y**2 + q.z**2) == 1)
+        self.assertTrue(q == Quaternion(-1.0, 0.0, 0.0, 0.0))
         
 if __name__ == '__main__':
     unittest.main()
