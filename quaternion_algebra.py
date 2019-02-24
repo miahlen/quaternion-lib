c02872006c885bf2b457379dd10db674ff1e2f72
miahlen <miahlen@gmail.com>
2019-02-24 22:12:17 +0100

    Add function multiply_quaternions to quaternion_algebra.py

--- quaternion_algebra.py
+++ quaternion_algebra.py
@@ -21,3 +21,11 @@ def normalize_quaternion(q):
     q.y /= norm
     q.z /= norm
     return q
+
+def multiply_quaternions(q1, q2):
+    q_out = Quaternion()
+    q_out.w = q1.w*q2.w  -  q1.x*q2.x  -  q1.y*q2.y  -  q1.z*q2.z
+    q_out.x = q1.w*q2.x  +  q1.x*q2.w  +  q1.y*q2.z  -  q1.z*q2.y
+    q_out.y = q1.w*q2.y  -  q1.x*q2.z  +  q1.y*q2.w  +  q1.z*q2.x
+    q_out.z = q1.w*q2.z  +  q1.x*q2.y  -  q1.y*q2.x  +  q1.z*q2.w
+    return q_out
