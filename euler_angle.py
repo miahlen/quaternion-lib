4e67acd139ebe5be7cab07c84461b32a31a46139
miahlen <miahlen@gmail.com>
2019-02-24 16:06:06 +0100

    Add functionality to set init values of class attributes at instantiation of EulerAngle class

--- euler_angle.py
+++ euler_angle.py
@@ -1,5 +1,5 @@
 class EulerAngle():
-    def __init__(self):
-        self.roll = 0.0
-        self.pitch = 0.0
-        self.yaw = 0.0
+    def __init__(self, roll=0.0, pitch=0.0, yaw=0.0):
+        self.roll = roll
+        self.pitch = pitch
+        self.yaw = yaw
