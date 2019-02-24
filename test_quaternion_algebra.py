3f6b2ccbc98e30c3a5e328be1a3fdb1356e91ca5
miahlen <miahlen@gmail.com>
2019-02-24 15:36:40 +0100

    Create test_quaternion_algebra.py
    Module to test quaternion algebra module

---
+++ test_quaternion_algebra.py
@@ -0,0 +1,10 @@
+import unittest
+import quaternion_algebra
+
+class TestQuaternionAlgebra(unittest.TestCase):
+
+    def test_normalize_quaternion(self):
+        raise NotImplementedError
+        
+if __name__ == '__main__':
+    unittest.main()
