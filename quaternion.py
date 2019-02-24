1ab28dc2f4c57b53a1aaecfc84b48f6f4f27c4f0
miahlen <miahlen@gmail.com>
2019-02-24 16:04:53 +0100

    Add functionality to set init values of class attributes at instantiation of quaternion class

--- quaternion.py
+++ quaternion.py
@@ -1,6 +1,6 @@
 class Quaternion():
-    def __init__(self):
-        self.w = 1.0
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
+    def __init__(self, w=1.0, x=0.0, y=0.0, z=0.0):
+        self.w = w
+        self.x = x
+        self.y = y
+        self.z = z
