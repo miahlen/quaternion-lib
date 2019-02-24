e1e04a1b9b140867be74bb0101c5043f0fd1ab86
miahlen <miahlen@gmail.com>
2019-02-24 17:29:47 +0100

    Add function get_quaternion_norm to quaternion_algebra.py
    Calling the method with a quaternion as input will return the norm of the quaternion

--- quaternion_algebra.py
+++ quaternion_algebra.py
@@ -1,3 +1,7 @@
 from quaternion import Quaternion
 from position import Position
 from euler_angle import EulerAngle
+import numpy as np
+
+def get_quaternion_norm(q):
+    return np.sqrt(q.w**2 + q.x**2 + q.y**2 + q.z**2)
