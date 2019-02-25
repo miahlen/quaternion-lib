170bd778ace0eaae60d301f9a825f42c7926f05e
miahlen <miahlen@gmail.com>
2019-02-25 14:10:53 +0100

    Add test test_get_quaternion_from_equal_vectors to test_quaternion_algebra.py
    To test implementation of corresponding function get_quaternion_from_vectors in quaternion_algebra.py (to be implemented)

--- test_quaternion_algebra.py
+++ test_quaternion_algebra.py
@@ -134,6 +134,20 @@ class TestQuaternionAlgebra(unittest.TestCase):
         # Assert excepted behaviour to the third decimal
         self.assertAlmostEqual(q, q_oracle)
 
+    def test_get_quaternion_from_equal_vectors(self):
+        # Input variables to function
+        u = [0.1, -0.2, 0.9]
+        v = [0.1, -0.2, 0.9]
+
+        # Function call
+        q = quaternion_algebra.get_quaternion_from_vectors(u, v)
+
+        # The expected output
+        q_oracle = Quaternion(1, 0, 0, 0)
+
+        # Assert excepted behaviour to the third decimal
+        self.assertAlmostEqual(q, q_oracle)
+
 
 if __name__ == '__main__':
     unittest.main()
