1a4a2162834005673af5db558890ff8775454511
miahlen <miahlen@gmail.com>
2019-02-25 11:49:09 +0100

    Add docstring to Quaternion class and init function

--- quaternion.py
+++ quaternion.py
@@ -1,5 +1,19 @@
 class Quaternion():
+    """ Form an Quaternion.
+
+    A class with instance variables w, x, y and z, representing
+    a quaternion. The w is the scalar part and (x,y,z) the vector
+    part of the quaternion.
+
+    Attributes:
+        w: A float representing the w component of the quaternion.
+        x: A float representing the x component of the quaternion.
+        y: A float representing the y component of the quaternion.
+        z: A float representing the z component of the quaternion.
+    """
+
     def __init__(self, w=1.0, x=0.0, y=0.0, z=0.0):
+        """Inits Quaternion class."""
         self.w = w
         self.x = x
         self.y = y
