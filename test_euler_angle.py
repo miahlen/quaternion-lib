6f3868c5bba5865838c48f10d9ec234b32e1e6af
Mikaela <miahlen@gmail.com>
2019-02-26 10:59:08 +0100

    Add method test_mul to test_euler_angle module
    To test operator overloading method __mul__ in euler_angle module

--- test_euler_angle.py
+++ test_euler_angle.py
@@ -14,6 +14,24 @@ class TestEulerAngle(unittest.TestCase):
         self.assertTrue(e1 == e2)
         self.assertFalse(e1 == e3)
 
+    def test_mul(self):
+        """Test operator overloading method __mul__"""
+        # Create instance of class EulerAngle to be multiplied by value
+        e_in = EulerAngle(0.9, -1, -0.4)
+
+        # Create value for EulerAngle instance to be multiplied by
+        val = 2.0
+
+        # Function call (to function to test)
+        e = e_in*val
+
+        # Create oracle
+        e_oracle = EulerAngle(0.9 * val, -1 * val, -0.4 * val)
+
+        # Assert expected behaviour
+        self.assertTrue(e == e_oracle)
+        self.assertFalse(e_in == e_oracle)
+
 
 if __name__ == '__main__':
     unittest.main()
