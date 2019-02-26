58b2eb869449aeb7b9ab751ef722e4d2dfc2c1cb
Mikaela <miahlen@gmail.com>
2019-02-26 10:43:31 +0100

    Create test_position.py
    Module to test position module

---
+++ test_position.py
@@ -0,0 +1,10 @@
+import unittest
+from position import Position
+
+class TestPosition(unittest.TestCase):
+
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
