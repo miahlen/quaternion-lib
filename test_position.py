3343ce22146a8f1ce6dedf0d142f973d00420ac9
Mikaela <miahlen@gmail.com>
2019-02-26 10:50:06 +0100

    Add method test_eq to test_position module
    To test operator overloading method __eq__ in position module

--- test_position.py
+++ test_position.py
@@ -3,8 +3,17 @@ from position import Position
 
 class TestPosition(unittest.TestCase):
 
-    def test(self):
-        self.assertTrue(True)
+    def test_eq(self):
+        """Test operator overloading method __eq__"""
+        # Create instances of class Position to compare
+        p1 = Position(0.9, -1, -0.4)
+        p2 = Position(0.9, -1, -0.4)
+        p3 = Position(0.9, -1.1, -0.4)
+
+        # Assert expected behaviour
+        self.assertTrue(p1 == p2)
+        self.assertFalse(p1 == p3)
+
 
 if __name__ == '__main__':
     unittest.main()
