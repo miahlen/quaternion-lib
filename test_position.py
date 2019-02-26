1837348aae939d7161ef11de74409ba416866453
Mikaela <miahlen@gmail.com>
2019-02-26 11:15:03 +0100

    Add method test_add to test_position module
    To test operator overloading method __add__ in position module

--- test_position.py
+++ test_position.py
@@ -14,6 +14,23 @@ class TestPosition(unittest.TestCase):
         self.assertTrue(p1 == p2)
         self.assertFalse(p1 == p3)
 
+    def test_add(self):
+        """Test operator overloading method __add__"""
+        # Create instances of class Position to add
+        p1 = Position(0.9, -1, -0.4)
+        p2 = Position(0.3, 1.3, 0.2)
+
+        # Function call (to function to test)
+        p = p1 + p2
+
+        # Create oracle
+        p_oracle = Position(1.2, 0.3, -0.2)
+
+        # Assert expected behaviour
+        self.assertAlmostEqual(p.x, p_oracle.x)
+        self.assertAlmostEqual(p.y, p_oracle.y)
+        self.assertAlmostEqual(p.z, p_oracle.z)
+
 
 if __name__ == '__main__':
     unittest.main()
