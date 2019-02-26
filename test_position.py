de31aae2ea93f3296c7a8a6f5da6b5d613f7db92
Mikaela <miahlen@gmail.com>
2019-02-26 11:17:03 +0100

    Add method test_sub to test_position module
    To test operator overloading method __sub__ in position module

--- test_position.py
+++ test_position.py
@@ -31,6 +31,23 @@ class TestPosition(unittest.TestCase):
         self.assertAlmostEqual(p.y, p_oracle.y)
         self.assertAlmostEqual(p.z, p_oracle.z)
 
+    def test_sub(self):
+        """Test operator overloading method __sub__"""
+        # Create instances of class Position to subtract
+        p1 = Position(0.9, -1, -0.4)
+        p2 = Position(0.3, 1.3, 0.2)
+
+        # Function call (to function to test)
+        p = p1 - p2
+
+        # Create oracle
+        p_oracle = Position(0.6, -2.3, -0.6)
+
+        # Assert expected behaviour
+        self.assertAlmostEqual(p.x, p_oracle.x)
+        self.assertAlmostEqual(p.y, p_oracle.y)
+        self.assertAlmostEqual(p.z, p_oracle.z)
+
 
 if __name__ == '__main__':
     unittest.main()
