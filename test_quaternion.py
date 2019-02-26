f03a1d998f0403ba32d2d0c15b08291cf90bca83
Mikaela <miahlen@gmail.com>
2019-02-26 10:43:54 +0100

    Create test_quaternion.py
    Module to test quaternion module

---
+++ test_quaternion.py
@@ -0,0 +1,10 @@
+import unittest
+from quaternion import Quaternion
+
+class TestQuaternion(unittest.TestCase):
+
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
