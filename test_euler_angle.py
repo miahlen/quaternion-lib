9bcf45d36c1d70f5beb97bf88743d0bc376c647d
Mikaela <miahlen@gmail.com>
2019-02-26 10:48:29 +0100

    Add method test_eq to test_euler_angle module
    To test operator overloading method __eq__ in euler_angle module

--- test_euler_angle.py
+++ test_euler_angle.py
@@ -3,8 +3,17 @@ from euler_angle import EulerAngle
 
 class TestEulerAngle(unittest.TestCase):
 
-    def test(self):
-        self.assertTrue(True)
+    def test_eq(self):
+        """Test operator overloading method __eq__"""
+        # Create instances of class EulerAngle to compare
+        e1 = EulerAngle(0.9, -1, -0.4)
+        e2 = EulerAngle(0.9, -1, -0.4)
+        e3 = EulerAngle(0.9, -1.1, -0.4)
+
+        # Assert expected behaviour
+        self.assertTrue(e1 == e2)
+        self.assertFalse(e1 == e3)
+
 
 if __name__ == '__main__':
     unittest.main()
