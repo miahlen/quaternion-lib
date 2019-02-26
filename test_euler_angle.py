1b756dac230cf8ea233cf540a864693f92e25968
Mikaela <miahlen@gmail.com>
2019-02-26 10:42:54 +0100

    Create test_euler_angle.py
    Module to test euler_angle module

---
+++ test_euler_angle.py
@@ -0,0 +1,10 @@
+import unittest
+from euler_angle import EulerAngle
+
+class TestEulerAngle(unittest.TestCase):
+
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
