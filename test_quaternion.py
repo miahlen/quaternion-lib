088b8e423184c102e65f8a940752c0cf1470418f
Mikaela <miahlen@gmail.com>
2019-02-26 10:52:02 +0100

    Add method test_eq to test_quaternion module
    To test operator overloading method __eq__ in quaternion module

--- test_quaternion.py
+++ test_quaternion.py
@@ -3,8 +3,17 @@ from quaternion import Quaternion
 
 class TestQuaternion(unittest.TestCase):
 
-    def test(self):
-        self.assertTrue(True)
+    def test_eq(self):
+        """Test operator overloading method __eq__"""
+        # Create instances of class Quaternion to compare
+        q1 = Quaternion(0.9, -1, -0.4, 0.1)
+        q2 = Quaternion(0.9, -1, -0.4, 0.1)
+        q3 = Quaternion(0.9, -1.1, -0.4, 0.1)
+
+        # Assert expected behaviour
+        self.assertTrue(q1 == q2)
+        self.assertFalse(q1 == q3)
+
 
 if __name__ == '__main__':
     unittest.main()
